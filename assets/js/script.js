55b5677f61462da5cbbbdb94ae1a7c0e6b6c53f0
jamiebird119 <jamie.bird119@gmail.com>
2020-04-20 08:42:21 +0000

    Added news stories and added script for on click interactivity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+$(".news").children("button").click(function(){
+    $(this).siblings("p").toggleClass("d-none");
+    $(this).siblings("img").toggleClass("d-none");
+    console.log("Click!");
+})
+
+
+
 // link to standings data (GET) --http://api.espn.com//sports/basketball/nba/rankings   HTTP https://www.espn.com/nba/standings
 
 //get Conference standings
