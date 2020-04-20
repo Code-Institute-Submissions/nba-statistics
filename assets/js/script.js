97cdc2c6c42758a93078eadd4d7a3fcc4c216c0a
jamiebird119 <jamie.bird119@gmail.com>
2020-04-20 09:47:04 +0000

    Added styling for mobile first on index.html current sections. Empty media queries set up for responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 $(".news").children("button").click(function(){
-    $(this).siblings("p").toggleClass("d-none");
-    $(this).siblings("img").toggleClass("d-none");
-    console.log("Click!");
+    $(this).siblings("div").toggleClass("d-none");
 })
 
+$(".news a").children("h4").mouseenter(function(){
+    $(this).addClass("underline");
+});
+$(".news a").children("h4").mouseleave(function(){
+    $(this).removeClass("underline");
+});
 
 
 // link to standings data (GET) --http://api.espn.com//sports/basketball/nba/rankings   HTTP https://www.espn.com/nba/standings
