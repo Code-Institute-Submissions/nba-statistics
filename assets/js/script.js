20398d02c946fb817a927bf46050eae352b3fa5e
jamiebird119 <jamie.bird119@gmail.com>
2020-04-24 11:29:32 +0000

    attached api for link team standings and found player ids for player search section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-$(".news").children("button").click(function(){
-    $(this).siblings("div").fadeToggle();
-})
 
-$(".news a").children("h4").mouseenter(function(){
-    $(this).addClass("underline");
+
+// link to standings data (GET) -- https://api-nba-v1.p.rapidapi.com/standings/%7Bleague%7D/%7Bseasonyear%7D/conference/%7Bconference%7D
+
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
 });
-$(".news a").children("h4").mouseleave(function(){
-    $(this).removeClass("underline");
-});
 
+xhr.open("GET", "https://api-nba-v1.p.rapidapi.com/standings/standard/2019");
+xhr.setRequestHeader("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "0a76bdc434msh890d7aed4a26f66p14aafejsnee71a10023f1");
 
-// link to standings data (GET) --http://api.espn.com//sports/basketball/nba/rankings   HTTP https://www.espn.com/nba/standings
+xhr.send(data);
 
-//get Conference standings
