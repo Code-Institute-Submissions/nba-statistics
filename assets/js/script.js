434eb3ea764fb546aa5f02f32995ccd4297ca435
jamiebird119 <jamie.bird119@gmail.com>
2020-04-27 14:29:01 +0000

    rearranged code with Ajax but not currently functional. . Ran out of HTTP requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,117 @@
 
 
 // link to standings data (GET) -- https://api-nba-v1.p.rapidapi.com/standings/%7Bleague%7D/%7Bseasonyear%7D/conference/%7Bconference%7D
-function getStandings(cb) {
+function getData(setData) {
+    $.ajax({
+        url: "https://api-nba-v1.p.rapidapi.com/standings/standard/2019/",
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+            "x-rapidapi-key": "0a76bdc434msh890d7aed4a26f66p14aafejsnee71a10023f1"
+        },
+        contentType: 'application/json; charset=utf-8',
+        success: function (result) {
+            setData(JSON.parse(this.responseText));
+            $.ajax({
+                url: "https://api-nba-v1.p.rapidapi.com/teams/league/standard",
+                type: 'GET',
+                dataType: 'json',
+                headers: {
+                    "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "0a76bdc434msh890d7aed4a26f66p14aafejsnee71a10023f1"
+                },
+                contentType: 'application/json; charset=utf-8',
+                success: function (result) {
+                    setData(JSON.parse(this.responseText));
+                },
+                error: function (error) {
+                    document.getElementById("teams").innerHtml = `<h2> Data not currently available - Please try again later. </h2>`
+                    console.log("failed");
+                }
+            });
 
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            cb(JSON.parse(this.responseText));
+        },
+        error: function (error) {
+            document.getElementById("teams").innerHtml = `<h2> Data not currently available - Please try again later. </h2>`
+            console.log("failed");
         }
     });
 
-    xhr.open("GET", "https://api-nba-v1.p.rapidapi.com/standings/standard/2019/");
-    xhr.setRequestHeader("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "0a76bdc434msh890d7aed4a26f66p14aafejsnee71a10023f1");
 
-    xhr.send();
-}
-function getTeams(cb) {
 
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            cb(JSON.parse(this.responseText));
-        }
-    });
-
-    xhr.open("GET", "https://api-nba-v1.p.rapidapi.com/teams/league/standard");
-    xhr.setRequestHeader("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "0a76bdc434msh890d7aed4a26f66p14aafejsnee71a10023f1");
-
-    xhr.send();
 }
 
+function setData(data, teams) {
 
-function writeToDocument(data, teams) {
+    var standings = [];
+    var teamsById = [];
+
+    teamsById = teams.api.teams;
+    return teamsById;
+
+    standings = data.api.standings;
+    return standings;
+
+}
+function writeToDocument(getData) {
     var el = document.getElementById("teams");
     el.innerHTML = "";
-    var standings = [];
-
-
-    getStandings(function (standings) {
-        standings = data.api.standings;
-        var content = "";
+    var content = "";
+    var teamsEast = [];
+    var teamsWest = [];
+    function setTeam(teamId, param) {
+        return getData(setData);
+        for (i = 0; i < teamsById.length;) {
+            if (teamsById[i]["teamId"] == teamId) {
+                var teamData = "";
+                teamData = teamsById[i][param];
+            }
+        }
+    }
+    for (ranking = 1; ranking < 16;) {
         var ranking = 1;
-        var teamsEast = [];
-        var teamsWest = [];
-        var teamsById = [];
-        function setTeams(teams, teamId, param) {
-            getTeams(function (teams) {
-               
-                teamsById = teams.api.teams;
-                return teamsById;
-            })
-            for (i=0;i<teamsById.length;){
-                if(teamsById[i]["teamId"] == teamId){
-                    var teamData = "";
-                    teamData = teamsById[i][param];
-                    return teamData;
-                }
+        for (i = 0; i < standings.length;) {
+            if (standings[i]["conference"]["name"] == "east" && standings[i]["conference"]["rank"] == ranking) {
+                var teams = [];
+                var teamId = standings[i]["teamId"];
+                teams.push(`<td>${standings[i]["conference"]["rank"]}</td>`);
+                teams.push(`<td> ${setTeam(teamId, "fullName")} <img src="${setTeam(teamId, "logo")}" alt="${setTeam(teamId, "shortName")}"/>
+                    </td>`);
+                teams.push(`<td>${standings[i]["win"]}</td>`);
+                teams.push(`<td>${standings[i]["loss"]}</td>`);
+                teams.push(`<td>${standings[i]["winPercentage"]}</td>`);
+                teams.push(`<td>${standings[i]["gamesBehind"]}</td>`);
+                teams.push(`<td>${standings[i]["home"]["win"]} - ${standings[i]["home"]["loss"]}</td>`);
+                teams.push(`<td>${standings[i]["away"]["win"]} - ${standings[i]["away"]["loss"]}</td>`);
+                teams.push(`<td>${standings[i]["lastTenWin"]} - ${standings[i]["lastTenLoss"]}</td>`);
+                teamsEast.push(`<tr> ${teams} </tr>`);
+                i = 0;
+                ranking++;
+            } if (standings[i]["conference"]["name"] == "west" && standings[i]["conference"]["rank"] == ranking) {
+                var teams = [];
+                var teamId = standings[i]["teamId"];
+                teams.push(`<td>${standings[i]["conference"]["rank"]}</td>`);
+                teams.push(`<td> ${setTeam(teamId, "fullName")} <img src="${setTeam(teamId, "logo")}" alt="${setTeam(teamId, "shortName")}"/>
+                    </td>`);
+                teams.push(`<td>${standings[i]["win"]}</td>`);
+                teams.push(`<td>${standings[i]["loss"]}</td>`);
+                teams.push(`<td>${standings[i]["winPercentage"]}</td>`);
+                teams.push(`<td>${standings[i]["gamesBehind"]}</td>`);
+                teams.push(`<td>${standings[i]["home"]["win"]} - ${standings[i]["home"]["loss"]}</td>`);
+                teams.push(`<td>${standings[i]["away"]["win"]} - ${standings[i]["away"]["loss"]}</td>`);
+                teams.push(`<td>${standings[i]["lastTenWin"]} - ${standings[i]["lastTenLoss"]}</td>`);
+                teamsWest.push(`<tr> ${teams} </tr>`);
+                i = 0;
+                ranking++;
+                console.log(teamsWest);
+            } else {
+                i++;
             }
-            
-            console.log(teamsById);
+
         };
-        for (ranking = 1; ranking < 16;) {
-            for (i = 0; i < standings.length;) {
-                if (standings[i]["conference"]["name"] == "east" && standings[i]["conference"]["rank"] == ranking) {
-                    var teams = [];
-                    var teamId = standings[i]["teamId"];
-                    teams.push(`<td>${standings[i]["conference"]["rank"]}</td>`);
-                    teams.push(`<td> ${setTeams(teamId,"fullName")} <img src="${setTeams(teamId,"logo")}" alt="${setTeams(teamId,"shortName")}"/>
-                    </td>`);
-                    teams.push(`<td>${standings[i]["win"]}</td>`);
-                    teams.push(`<td>${standings[i]["loss"]}</td>`);
-                    teams.push(`<td>${standings[i]["winPercentage"]}</td>`);
-                    teams.push(`<td>${standings[i]["gamesBehind"]}</td>`);
-                    teams.push(`<td>${standings[i]["home"]["win"]} - ${standings[i]["home"]["loss"]}</td>`);
-                    teams.push(`<td>${standings[i]["away"]["win"]} - ${standings[i]["away"]["loss"]}</td>`);
-                    teams.push(`<td>${standings[i]["lastTenWin"]} - ${standings[i]["lastTenLoss"]}</td>`);
-                    teamsEast.push(`<tr> ${teams} </tr>`);
-                    i = 0;
-                    ranking++;
-                } else {
-                    i++;
-                }
-
-            }
-
-        }
-        for (ranking = 1; ranking < 16;) {
-            for (i = 0; i < standings.length;) {
-                if (standings[i]["conference"]["name"] == "west" && standings[i]["conference"]["rank"] == ranking) {
-                    var teams = [];
-                    var teamId = standings[i]["teamId"];
-                     teams.push(`<td>${standings[i]["conference"]["rank"]}</td>`);
-                    teams.push(`<td> ${setTeams(teamId,"fullName")} <img src="${setTeams(teamId,"logo")}" alt="${setTeams(teamId,"shortName")}"/>
-                    </td>`);
-                    teams.push(`<td>${standings[i]["win"]}</td>`);
-                    teams.push(`<td>${standings[i]["loss"]}</td>`);
-                    teams.push(`<td>${standings[i]["winPercentage"]}</td>`);
-                    teams.push(`<td>${standings[i]["gamesBehind"]}</td>`);
-                    teams.push(`<td>${standings[i]["home"]["win"]} - ${standings[i]["home"]["loss"]}</td>`);
-                    teams.push(`<td>${standings[i]["away"]["win"]} - ${standings[i]["away"]["loss"]}</td>`);
-                    teams.push(`<td>${standings[i]["lastTenWin"]} - ${standings[i]["lastTenLoss"]}</td>`);
-                    i = 0;
-                    ranking++;
-                } else {
-                    i++;
-                }
-
-            }
-
-        }
-        el.innerHTML = `<table>
+    }
+    el.innerHTML = `<table class="conference-standings>
         <tr> <th>Eastern Conference</th> </tr>
         <tr>
         <th> Rank </th>
@@ -132,7 +128,7 @@ function writeToDocument(data, teams) {
         ${teamsWest}
         </table>`;
 
-    });
 }
 
-getStandings(writeToDocument);
+
+
