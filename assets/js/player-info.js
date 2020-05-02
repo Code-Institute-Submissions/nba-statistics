efeeafed90e6b2ac538e9182ba0b54fb943d61c3
jamiebird119 <jamie.bird119@gmail.com>
2020-05-02 11:14:49 +0000

    Added search submit button and finished writing player data to document - needs styling

--- assets/js/player-info.js
+++ assets/js/player-info.js
@@ -1,15 +1,14 @@
 
-$(document).ready(function () {
-    fetchPlayerInfo("Alex Abrines");
-})
 
 
-function fetchPlayerInfo(playerName) {
+function fetchPlayerInfo(event) {
+    var playerName = $("#player-name").val();
+
     getPlayers(playerName);
 
     $("#nba-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loading.gif" alt="loading..." />
+            <img src="./images/loading.gif" alt="loading..." />
         </div>`);
 }
 
@@ -47,13 +46,14 @@ function playerRequest(all_players, playerName) {
             returnPlayerStats(playerId);
             break;
         } else {
-            $("#nba-user-data").html("<p> Player not found - please enter another player or check the spelling </p>");
+            setTimeout(function(){ $("#nba-user-data").html("<p> Player not found - please enter another player or check the spelling </p>");},8000)
+           
         }
     }
 
 }
 
-function returnPlayerStats(playerId) {
+function returnPlayerStats(playerId, playerName) {
 
     $.ajax({
         url: "https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/" + playerId,
@@ -70,20 +70,28 @@ function returnPlayerStats(playerId) {
                 playerStats[i] = {
                     points: Number(results.api.statistics[i]["points"]),
                     rebounds: Number(results.api.statistics[i]["totReb"]),
-                    assits: Number(results.api.statistics[i]["assists"]),
+                    assists: Number(results.api.statistics[i]["assists"]),
                     fgp: Number(results.api.statistics[i]["fgp"]),
-                    tpp: Number(results.api.statistics[i]["ttp"]),
+                    tpp: Number(results.api.statistics[i]["tpp"]),
                     ftp: Number(results.api.statistics[i]["ftp"]),
                     steals: Number(results.api.statistics[i]["steals"]),
                     blocks: Number(results.api.statistics[i]["blocks"]),
                     turnovers: Number(results.api.statistics[i]["turnovers"])
                 }
             }
-            console.log(playerStats);
             var playerAvg = {};
-            var points, rebounds, assists, fgp, tpp, ftp, steals, blocks, turnovers = 0;
-
+            var points = 0;
+            var rebounds = 0;
+            var assists = 0;
+            var fgp = 0;
+            var tpp = 0;
+            var ftp = 0;
+            var steals= 0;
+            var blocks = 0;
+            var turnovers = 0;
+            var gamesPlayed = 0;
             for (i in playerStats) {
+                gamesPlayed += 1;
                 points += playerStats[i]["points"];
                 rebounds += playerStats[i]["rebounds"];
                 assists += playerStats[i]["assists"];
@@ -93,13 +101,56 @@ function returnPlayerStats(playerId) {
                 steals += playerStats[i]["steals"];
                 blocks += playerStats[i]["blocks"];
                 turnovers += playerStats[i]["turnovers"];
-                console.log(points);
             }
-            
-
+            playerAvg[0] = {
+                points: (points/gamesPlayed).toFixed(2),
+                rebounds: (rebounds/gamesPlayed).toFixed(2),
+                assists: (assists/gamesPlayed).toFixed(2),
+                fgp: (fgp/gamesPlayed).toFixed(2),
+                tpp: (tpp/gamesPlayed).toFixed(2),
+                ftp: (ftp/gamesPlayed).toFixed(2),
+                steals: (steals/gamesPlayed).toFixed(2),
+                blocks: (blocks/gamesPlayed).toFixed(2),
+                turnovers: (turnovers/gamesPlayed).toFixed(2)
+            }
+            console.log(playerAvg);
+            writeToDocument(playerAvg, playerName);
+        
         },
         error: function (error) {
 
         }
     });
+}
+function writeToDocument(playerAvg, playerName){
+    var el = document.getElementById("nba-user-data");
+
+    el.innerHTML = `<h2> Per Game Averages </h2>
+    <table>
+    <tr>
+    <th> Player Name </th>
+    <th> Points </th>
+    <th> Rebounds </th>
+    <th> Assists </th>
+    <th> Field Goal Percentage </th>
+    <th> Three Point Percentage </th>
+    <th> Free Throw Percentage </th>
+    <th> Steals </th>
+    <th> Blocks </th>
+    <th> Turnovers </th>
+    </tr>
+    <tr> 
+    <td> ${$("#player-name").val()} </td>
+    <td> ${playerAvg[0]["points"]} </td>
+    <td> ${playerAvg[0]["rebounds"]} </td>
+    <td> ${playerAvg[0]["assists"]} </td>
+    <td> ${playerAvg[0]["fgp"]} </td>
+    <td> ${playerAvg[0]["tpp"]} </td>
+    <td> ${playerAvg[0]["ftp"]} </td>
+    <td> ${playerAvg[0]["steals"]} </td>
+    <td> ${playerAvg[0]["blocks"]}</td>
+    <td> ${playerAvg[0]["turnovers"]}</td>
+     </tr>
+
+    </table>`
 }
