4632ba95fef84f4888e0280500968fbba2fa0b79
jamiebird119 <jamie.bird119@gmail.com>
2020-05-05 10:57:16 +0000

    Autocomplete function added, bug with player data request

--- assets/js/player-info.js
+++ assets/js/player-info.js
@@ -1,9 +1,113 @@
+$(document).ready(function () {
+    autocomplete(document.getElementById("player-name"));
+})
+
+
+// autocomplete function from https://www.w3schools.com/howto/howto_js_autocomplete.asp - edited
+function autocomplete(inp) {
+
+    var names = []
+    $.ajax({
+        url: "https://api-nba-v1.p.rapidapi.com/players/league/standard",
+        method: 'GET',
+        dataType: 'json',
+        headers: {
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+            "x-rapidapi-key": "0a76bdc434msh890d7aed4a26f66p14aafejsnee71a10023f1"
+        },
+        contentType: 'application/json; charset=utf-8',
+        success: function (results) {
+            for (i = 0; i < results.api.players.length; i++) {
+                names[i] = results.api.players[i]["firstName"] + " " + results.api.players[i]["lastName"]
+                
+            }
+            return names;
+        },
+        error: function (error) {
+            console.log("error");
+        }
+
+    });
+    
+    var currentFocus;
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+
+        if (!val) { return false; }
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+        for (i = 0; i < names.length; i++) {
+            if (names[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + names[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += names[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + names[i] + "'>";
+                b.addEventListener("click", function (e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        } else if (e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    // autocomplete function from https://www.w3schools.com/howto/howto_js_autocomplete.asp - edited
+    function addActive(x) {
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    document.addEventListener("click", function (e) {
+        if (e.target == $("#submit")){
+            fetchPlayerInfo();
+        }else{
+        closeAllLists(e.target);
+        }
+    });
+}
+
 
 
 
 function fetchPlayerInfo(event) {
     var playerName = $("#player-name").val().toLowerCase();
-
     getPlayers(playerName);
 
     $("#loader").html(
@@ -27,7 +131,6 @@ function getPlayers(playerName) {
         contentType: 'application/json; charset=utf-8',
         success: function (results) {
             var all_players = [];
-            console.log(results);
             for (i = 0; i < results.api.players.length; i++) {
                 all_players[results.api.players[i]["playerId"]] = {
                     playerId: results.api.players[i]["playerId"],
@@ -40,7 +143,7 @@ function getPlayers(playerName) {
 
         },
         error: function (error) {
-
+            console.log(error);
         }
     });
 }
